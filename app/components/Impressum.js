42df2e5b2c5437b7027615b51e2f16f626cd8ac6
Thomas Brüggemann <mail@thomasbrueggemann.com>
2016-01-02 00:56:57 +0100

    Impressu legal disclaimer update

--- app/components/Impressum.js
+++ app/components/Impressum.js
@@ -23,7 +23,7 @@ class Impressum extends React.Component {
 						<p>
                         The following information (Impressum) is required under German law.</p>
 
-                        <p>Responsible for the content of this site:</p>
+                        <p>Responsible for this site:</p>
 
                         <p><span className="reverse">nnameggürB samohT</span><br />
                         <span className="reverse">51 eßartsreliewtdiehcS</span><br />
@@ -32,7 +32,7 @@ class Impressum extends React.Component {
 						</p>
 
                         <h3>Legal disclaimer</h3>
-                        <p>The contents of these pages were prepared with utmost care. Nonetheless, I cannot assume liability for the timeless accuracy and completeness of the information. My website contains links to external websites. As the contents of these third-party websites are beyond my control, I cannot accept liability for them. ResponsibiIity for the contents of the linked pages is always held by the provider or operator of the pages.</p>
+                        <p>The contents of these pages are generated automatically based on the YouTube channel title, description and videos of the individual channels listed. The creator of this website has no influence on the content of the channel descriptions or other channel related texts. Also, The creator of this website cannot assume liability for the timeless accuracy and completeness of the information. This website contains links to external websites. As the contents of these third-party websites are beyond my control, I cannot accept liability for them. ResponsibiIity for the contents of the linked pages is always held by the provider or operator of the pages.</p>
 
                         <h3>Data protection</h3>
                         <p>In general, when visiting the website of “Sailing Channels”, no personal data are saved. No data will be passed on to third parties without your consent. We point out that in regard to unsecured data transmission in the internet (e.g. via email), security cannot be guaranteed. Such data could possibIy be accessed by third parties.</p>
