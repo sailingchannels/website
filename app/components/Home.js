d0d0fb28cd28b5ac5885e71c0f437a87f65420f9
Thomas Brüggemann <mail@thomasbrueggemann.com>
2016-02-18 16:03:35 +0100

    Removed Scrollup Button for now due to performance issues

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,60 +8,16 @@ import Banner from "./Banner";
 
 class Home extends React.Component {
 
-	// CONSTRUCTOR
-	constructor(props) {
-		super(props);
-		this.state = {
-			"showScrollUp": false
-		};
-	}
-
-	// COMPONENT DID MOUNT
-	componentDidMount() {
-		$(window).on("scroll", this.onWindowScroll.bind(this));
-	}
-
-	// COMPONENT WILL UNMOUNT
-	componentWillUnmount() {
-		$(window).off("scroll");
-	}
-
-	// ON WINDOW SCROLL
-	onWindowScroll() {
-
-		// did the scrollup state change?
-		if(this.state.showScrollUp !== ($(window).scrollTop() > 100)) {
-
-			// do or do not show scrollup button
-			this.setState({
-				"showScrollUp": ($(window).scrollTop() > 100)
-			});
-		}
-	}
-
-	// SCROLL UP
-	scrollUp() {
-		window.scrollTo(0, 0);
-	}
-
 	// RENDER
 	render() {
 
 		var sortBy = this.props.params.sortBy || "subscribers";
-		var scrollUp = null;
-
-		if(this.state.showScrollUp === true) {
-			scrollUp = <div className="scroll-up-btn" title="Scroll to top" onClick={this.scrollUp.bind(this)}>
-				<i className="fa fa-arrow-circle-up fa-3x"></i>
-			</div>;
-		}
 
 		return (
 			<div className="container">
 				<OffsetSocial />
 				<Logo />
 				<OffsetMenu />
-				{scrollUp}
 				<SearchBar sortBy={sortBy} history={this.props.history} />
 				<ChannelList sortBy={sortBy} history={this.props.history} />
 				{this.props.children}
