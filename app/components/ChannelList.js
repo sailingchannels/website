43a312e29532b2b66e482b8d840c5e2ee2c5bd1f
Thomas Brüggemann <mail@thomasbrueggemann.com>
2016-03-05 21:58:37 +0100

    using react-infinite

--- app/components/ChannelList.js
+++ app/components/ChannelList.js
@@ -3,6 +3,7 @@ import ChannelListItem from "./ChannelListItem";
 import ChannelActions from "../actions/ChannelActions";
 import ChannelStore from "../stores/ChannelStore";
 import {Navigation} from "react-router";
+import Infinite from "react-infinite";
 
 class ChannelList extends React.Component {
 
@@ -19,7 +20,7 @@ class ChannelList extends React.Component {
 
 		// handle event bus page changes
 		$(window).on("changeSort", this.changeSort.bind(this));
-		$(window).on("scroll", this.scrollWindow.bind(this));
+		//$(window).on("scroll", this.scrollWindow.bind(this));
 		$(window).on("rerenderList", this.rerenderList.bind(this));
 
 		var sortBy = this.props.sortBy;
@@ -33,7 +34,7 @@ class ChannelList extends React.Component {
 
 	// COMPONENT WILL UNMOUNT
 	componentWillUnmount() {
-		$(window).off("scroll");
+		//$(window).off("scroll");
 		$(window).off("changeSort");
 		$(window).off("rerenderList");
 
@@ -73,17 +74,18 @@ class ChannelList extends React.Component {
 	// SCROLL WINDOW
 	scrollWindow() {
 
-		var scrollBottomThreshold = 150;
+		/*var scrollBottomThreshold = 150;
 		if ($(window).scrollTop() + $(window).height() > $(document).height() - scrollBottomThreshold &&
 			this.state.loading === false)
-		{
+		{*/
+		if(this.state.loading === false) {
 		   this.setState({
 			   "loading": true
 		   });
 
 		   // load more data
 		   ChannelActions.getChannels(this.props.sortBy, this.state.skip + this.state.take, 25);
-	   	}
+	  	}
 	}
 
 	// RERENDER LIST
@@ -125,10 +127,14 @@ class ChannelList extends React.Component {
             <div className="row">
 				<div className="col-md-1"></div>
                 <div className="col-md-10">
-                    {this.state.channels.map(c => (
-        				<ChannelListItem key={"cli-" + c.id} channel={c} sortBy={this.state.sortBy} />
-        			))}
-					{(this.state.loading === true) ? <center className="loadMore">Loading more channels ...</center> : null}
+					<Infinite useWindowAsScrollContainer={true} elementHeight={230} infiniteLoadBeginEdgeOffset={230} onInfiniteLoad={this.scrollWindow.bind(this)}>
+
+	                    {this.state.channels.map(c => (
+	        				<ChannelListItem key={"cli-" + c.id} channel={c} sortBy={this.state.sortBy} />
+	        			))}
+
+					</Infinite>
+
                 </div>
 				<div className="col-md-1"></div>
             </div>
--- app/components/ChannelListItem.js
+++ app/components/ChannelListItem.js
@@ -20,20 +20,22 @@ class ChannelListItem extends React.Component {
 	render() {
 		return (
 			<div className="row channel-row">
-                <div className="col-md-2 col-xs-2">
+                <div className="col-md-2 col-sm-2 col-xs-3">
 					<center>
-                    	<img src="https://cdn.rawgit.com/thomasbrueggemann/sailing-channels/master/public/img/spacer.png" data-src={this.props.channel.thumbnail} className="channel-thumb" />
+                    	<img src={this.props.channel.thumbnail} className="channel-thumb" />
 					</center>
                 </div>
-                <div className="col-md-7 col-xs-10">
+                <div className="col-md-7 col-sm-6 col-xs-9">
                     <h3>
                         <Link to={"/channel/" + this.props.channel.id}>
                             {this.props.channel.title}
                         </Link>
                     </h3>
-					<Description text={this.props.channel.description} maxLength={350} />
+					<div className="hidden-xs">
+						<Description text={this.props.channel.description} maxLength={300} />
+					</div>
                 </div>
-                <div className="col-md-3 col-xs-10 col-xs-offset-2 col-md-offset-0">
+                <div className="col-md-3 col-sm-4 col-xs-10 col-xs-offset-3 col-md-offset-0 col-sm-offset-0">
 
 					{(this.props.channel.subscribersHidden === true) ? <p className="text-warning"><b>Subscriber info hidden <i className="fa fa-frown-o"></i></b></p> : null}
                     {(this.props.channel.subscribersHidden === false) ? <p><b>Subscribers:</b> {this.props.channel.subscribers.toLocaleString()}</p> : null}
@@ -42,7 +44,7 @@ class ChannelListItem extends React.Component {
 					{(this.props.channel.lastUploadAt) ? <p><b>Last upload:</b> {$.timeago(new Date(this.props.channel.lastUploadAt * 1000))}</p> : ""}
 					<p><b>Founded:</b> {this.formatDate(this.props.channel.publishedAt)}</p>
 
-					<a target="_blank" href={"https://youtube.com/channel/" + this.props.channel.id + "?sub_confirmation=1"} className="btn btn-danger btn-sidebar btn-raised">
+					<a target="_blank" href={"https://youtube.com/channel/" + this.props.channel.id + "?sub_confirmation=1"} className="hidden-xs btn btn-danger btn-sidebar btn-raised">
 						<i className="fa fa-youtube-play"></i> Subscribe
 					</a>
                 </div>
