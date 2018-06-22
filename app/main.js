6faf98e3080eb01c1346e2b9c90807a84bfcc022
Thomas Brüggemann <mail@thomasbrueggemann.com>
2018-06-23 00:11:59 +0200

    rebuild

--- app/main.js
+++ app/main.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import OwnBrowserHistory from "helpers/ownBrowserHistory";
 import App from "components/App";
