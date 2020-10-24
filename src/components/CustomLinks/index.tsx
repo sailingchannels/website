6cbf9559e7d9535c1330828510dbdface48645e5
Thomas Brüggemann <mail@thomasbrueggemann.com>
2020-10-24 13:05:57 +0200

    adjust custom link url

--- src/components/CustomLinks/index.tsx
+++ src/components/CustomLinks/index.tsx
@@ -14,9 +14,16 @@ function CustomLinks(props: CustomLinksProps) {
 				</h3>
 				<ul>
 					{props.links.map((link: CustomLink) => {
+
+						let l = link.uRL;
+						if (!link.uRL.startsWith("http"))
+						{
+							l = `https://youtube.com${link.uRL}`;
+						}
+
 						return (
 							<li key={link.uRL}>
-								<a href={`https://youtube.com${link.uRL}`} target="_blank">
+								<a href={l} target="_blank">
 									<img alt={link.title} src={link.icon} /> {link.title}
 								</a>
 							</li>
